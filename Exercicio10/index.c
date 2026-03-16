f832a54601104da5aad7a6703377dfa8a6b426b4
junior <junior.oliveira05@icloud.com>
2026-03-16 18:42:15 -0300

    add 10 exercicio

---
+++ Exercicio10/index.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    /*10- Dado a formula Y = X + 5 escrever o valor
+    de Y quando dado uma entrada de X..
+    */
+    int x, y;
+    printf("Digite o valor de X: ");
+    scanf("%d", &x);
+    y = x + 5;
+    printf("O valor de Y é: %d\n", y);
+    return 0;
+}
