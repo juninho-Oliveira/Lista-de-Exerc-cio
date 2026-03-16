dc3a21ab90f232cc75db89d7b220c3f83f5f264c
junior <junior.oliveira05@icloud.com>
2026-03-16 18:17:15 -0300

    add primeiro exercicio

---
+++ Exercicio1/index.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    /*1- Escreva um programa para escrever quando inicializado 
+    “Olá aluno hoje é sexta-feira” */
+    printf("Olá aluno hoje é sexta-feira\n");
+    return 0;
+}
